e3d2b4cd1b1ea8bc975b5340ae1eba1d523ed5c1
william8815 <williamhsu88157976@gmail.com>
2022-07-29 23:50:31 +0800

    修改設定頁面 navbar 比例不同問題

--- src/views/AccountEdit.vue
+++ src/views/AccountEdit.vue
@@ -1,73 +1,86 @@
 <template>
-  <div class="accountEdit">
+  <div class="container row">
     <!--UserSidbar -->
-    <UserSidbar />
-  <div class="main">
-          <div class="main__title">
-        <h1>帳戶設定</h1>
-      </div>
+    <!-- <UserSidbar /> -->
+    <!-- navbar -->
+    <section class="col-xl-2 col-lg-2">
+      <Navbar />
+    </section>
+    <section class="col-xl-7 col-lg-7">
+      <div class="main">
+        <div class="main__title">
+          <h1>帳戶設定</h1>
+        </div>
 
-      <div class="main__body">
-        <!-- AccountEditForm -->
-        <AccountEditForm 
-        :initial-user="user"
-        @handle-after-submit="handleAfterSubmit" />
+        <div class="main__body">
+          <!-- AccountEditForm -->
+          <AccountEditForm
+            :initial-user="user"
+            @handle-after-submit="handleAfterSubmit"
+          />
+        </div>
       </div>
-  </div>
+    </section>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-import UserSidbar from '../components/UserSidbar.vue'
-import AccountEditForm from '../components/AccountEditForm.vue'
+import UserSidbar from "../components/UserSidbar.vue";
+import AccountEditForm from "../components/AccountEditForm.vue";
+import Navbar from "./../components/Navbar.vue";
 // import Navbar from '../components/Navbar.vue'
 export default {
-  name: 'AccountEdit',
+  name: "AccountEdit",
   components: {
     AccountEditForm,
     UserSidbar,
+    Navbar,
   },
   data() {
     return {
       user: {
-        name: '',
-        account: '',
-        email: '',
-        password: '',
-        passwordCheck: ''
-      }
-    }
+        name: "",
+        account: "",
+        email: "",
+        password: "",
+        passwordCheck: "",
+      },
+    };
   },
   methods: {
     handleAfterSubmit(formData) {
-      for(let [name, value] of formData.entries()) {
-        console.log(name + ": " + value)
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ": " + value);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.accountEdit {
-  display: flex;
+.container {
+  // display: flex;
+  max-width: 1144px;
+  height: 100vh;
+  margin: 0 auto;
 }
-
+// 取消滾輪
+::-webkit-scrollbar {
+  /* make scrollbar transparent */
+  width: 0px;
+  background: transparent;
+}
 .main {
   height: 100vh;
   border-left: 1px solid #e6ecf0;
   border-right: 1px solid #e6ecf0;
-  margin-left: 24px;
-  padding-left: 15px;
-  width: auto;
-  
+  // width: auto;
 }
 .main__title {
-  margin-top: 24px;
   border-bottom: 1px solid#E6ECF0;
   h1 {
-    padding: 0 8px 25px;
+    padding: 24px;
     font-weight: 700;
     font-size: 24px;
     line-height: 26px;
@@ -75,10 +88,9 @@ export default {
 }
 
 .main__body {
-  margin-top: 1rem;
+  padding: 24px 23px 0;
   display: flex;
   flex-wrap: wrap;
-  max-height: 100vh;
+  // max-height: 100vh;
 }
-
 </style>
