988b2cb5aa38bf81afb1242cc3f05253be5c44e1
william8815 <williamhsu88157976@gmail.com>
2022-07-30 18:05:38 +0800

    修改 推文清單與 navbar 連結統一

--- src/views/AdminPost.vue
+++ src/views/AdminPost.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="adminPost">
+  <div class="container row">
     <!-- AdminSidbar -->
-    <AdminSidbar />
-    <div class="main">
-      <div class="main__title">
-        <h1>推文清單</h1>
+    <!-- <AdminSidbar /> -->
+    <!-- navbar -->
+    <section class="col-xl-2 col-lg-2">
+      <Navbar />
+    </section>
+    <!-- main -->
+    <section class="col-xl-10 col-lg-10">
+      <div class="main">
+        <div class="main__title">
+          <h1>推文清單</h1>
+        </div>
+        <div class="main__body">
+          <!-- AdminTweetsList -->
+          <AdminTweetsList
+            v-for="tweet in tweets"
+            :key="tweet.id"
+            :tweet="tweet"
+            @after-delete-tweet="afterDeleteTweet"
+          />
+        </div>
       </div>
-      <div class="main__body">
-        <!-- AdminTweetsList -->
-        <AdminTweetsList 
-        v-for="tweet in tweets" 
-        :key="tweet.id" 
-        :tweet="tweet" 
-        @after-delete-tweet="afterDeleteTweet"
-        />
-      </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -163,11 +170,13 @@ const dammyData = {
 };
 import AdminSidbar from "../components/AdminSidbar.vue";
 import AdminTweetsList from "../components/AdminTweetsList.vue";
+import Navbar from "./../components/Navbar.vue";
 export default {
   name: "AdminPost",
   components: {
     AdminSidbar,
     AdminTweetsList,
+    Navbar,
   },
   data() {
     return {
@@ -179,8 +188,8 @@ export default {
       this.tweets = dammyData.tweets;
     },
     afterDeleteTweet(tweetId) {
-      this.tweets = this.tweets.filter(tweet => tweet.id !== tweetId)
-    }
+      this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+    },
   },
   created() {
     this.fetchTweets();
@@ -189,20 +198,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.adminPost {
-  display: flex;
-  margin-right: 130px;
-  max-height: 100vh;
-  width: auto;
+.container {
+  // display: flex;
+  // margin-right: 130px;
+  // max-height: 100vh;
+  // width: auto;
+  max-width: 1144px;
+  height: 100vh;
+  margin: 0 auto;
 }
 
 .main {
-  width: auto;
-  min-height: 100vh;
+  // width: auto;
+  height: 100vh;
   border-right: 1px solid #e6ecf0;
   border-left: 1px solid #e6ecf0;
-  margin-left: 24px;
+  // margin-left: 24px;
   overflow-y: scroll;
+  position: relative;
 }
 
 ::-webkit-scrollbar {
@@ -211,9 +224,11 @@ export default {
 }
 
 .main__title {
-  position: fixed;
-  width: 937px;
-  background: #FFF;
+  position: sticky;
+  top: 0;
+  // width: 937px;
+  width: 100%;
+  background: #fff;
   border-bottom: 1px solid#E6ECF0;
   h1 {
     padding: 24px;
@@ -223,9 +238,9 @@ export default {
   }
 }
 
-.main__body {
-  margin-top: 90px;
-  max-height: 100%;
-  width: 937px;
-}
+// .main__body {
+//   // margin-top: 90px;
+//   // max-height: 100%;
+//   // width: 937px;
+// }
 </style>
